cbfbf758b2c31731ddc4d960caa59ce83036dd8c
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-03 09:55:27 +0530

    Add perfect number checker in q3.py

---
+++ q3.py
@@ -0,0 +1,11 @@
+num = 6
+sum = 0
+i = 1
+while i < num:
+    if num % i == 0:
+        sum = sum + i
+    i = i + 1
+if (sum == num):
+    print("perfect number:")
+else:
+    print("not perfect number:")
